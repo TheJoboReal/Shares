6d296123cd150062772cebd3527aa507b0413245
TheJoboReal <kasper.jobo@gmail.com>
2024-04-25 15:35:54 +0200

    Initial push

---
+++ target/debug/deps/shares_r-2cee3342be0bbdb5.d
@@ -0,0 +1,5 @@
+/mnt/e/Drive/Civilingeniør_Robotteknologi/Rust/Shares/target/debug/deps/libshares_r-2cee3342be0bbdb5.rmeta: src/main.rs
+
+/mnt/e/Drive/Civilingeniør_Robotteknologi/Rust/Shares/target/debug/deps/shares_r-2cee3342be0bbdb5.d: src/main.rs
+
+src/main.rs:

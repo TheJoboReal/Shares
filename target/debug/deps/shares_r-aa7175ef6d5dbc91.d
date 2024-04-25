2dff7ac8d6ff4b5c0e5294076fb4565e7d43161f
TheJoboReal <kasper.jobo@gmail.com>
2024-04-25 15:42:27 +0200

    Added gitignore

--- target/debug/deps/shares_r-aa7175ef6d5dbc91.d
+++
@@ -1,5 +0,0 @@
-/mnt/e/Drive/Civilingeniør_Robotteknologi/Rust/Shares/target/debug/deps/libshares_r-aa7175ef6d5dbc91.rmeta: src/main.rs
-
-/mnt/e/Drive/Civilingeniør_Robotteknologi/Rust/Shares/target/debug/deps/shares_r-aa7175ef6d5dbc91.d: src/main.rs
-
-src/main.rs:

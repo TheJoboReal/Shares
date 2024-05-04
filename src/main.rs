93b80f3031b373749c2abaa416c5803fadcb2e18
TheJoboReal <kasper.jobo@gmail.com>
2024-05-04 14:14:37 +0200

    The beginning of ratatui

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,6 @@ use person::Person;
 use persons::Persons;
 
 fn main() {
-    let mut p1 = Person::new("John".to_string(), 150.0);
-    let mut p2 = Person::new("Kasper".to_string(), 40.0);
-    let mut p3 = Person::new("Runa".to_string(), 20.0);
-    let people = vec![p1, p2, p3];
 
-    let mut persons = Persons::new(people);
-
-    persons.print_persons();
-    persons.update_total_debt();
-    println!("Total debt: {}", persons.total_debt);
-
-    persons.update_person_share();
-    persons.print_persons();
-    persons.print_persons_share();
-
-    persons.what_owed();
 }
 
